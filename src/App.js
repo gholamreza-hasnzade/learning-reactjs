072aa891e2f4c6e72f77d0057daa8a1024eefe36
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-07 09:27:44 +0330

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div>
-      <p>this App Components</p>
+      <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Media from './Media'
+
+const Footer = () => {
+  return (
+    <div>
+        <hr />
+        <Media />
+        <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
