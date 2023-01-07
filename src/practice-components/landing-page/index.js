71d35b067e4d602dc1142bbb772066b2540b8de6
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-07 10:52:53 +0330

    fixed landing page

--- src/practice-components/landing-page/index.js
+++ src/practice-components/landing-page/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from './header/header'
+import React from "react";
+import Header from "./header/header";
+import Article from "./article/article";
 
 const LandingPage = () => {
-  return (
-    <>
-      <Header />
-    </>
-  )
-}
+    return (
+        <>
+            <Header />
+            <Article />
+        </>
+    );
+};
 
-export default LandingPage
+export default LandingPage;
