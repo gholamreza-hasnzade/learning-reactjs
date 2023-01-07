2073f05d73938987b67219ac8d4bb0a009bf9a39
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-07 08:55:47 +0330

    delete file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Footer from "./footer";
 
 const App = () => {
   return (
     <div>
       <p>this App Components</p>
-      <Footer />
     </div>
   );
 };
--- src/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>Footer Componnets</div>
-  )
-}
-
-export default Footer
