16fc91c7fbe3aa245c9c8dc542c744bf902abe15
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-07 09:56:34 +0330

    create navbar projects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+// eslint-disable-next-line jsx-a11y/anchor-is-valid
 
+import React from "react";
+
+// * import css
+import "./App.css";
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <div className="nav-background">
+                <div className="nav-container">
+                    <header className="nav-header">
+                        <h1>
+                            <a href="#" className="nav-logo">
+                                LOGO
+                            </a>
+                        </h1>
+                    </header>
+                    <nav>
+                        <ul className="nav-ul">
+                          <li> <a href="#" className="nav-link active-link">home</a></li>
+                          <li> <a href="#" className="nav-link">news</a></li>
+                          <li> <a href="#" className="nav-link">contact</a></li>
+                          <li> <a href="#" className="nav-link">about</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
