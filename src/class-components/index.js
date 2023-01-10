cbd9decc0c50d7f359c435e6bfc09f36f7baabe1
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-10 10:59:14 +0330

    temp in React cold and hot

--- src/class-components/index.js
+++ src/class-components/index.js
@@ -1,42 +1,14 @@
-import Counter from "./counter/counter";
-//import User from "./users/User";
 import React, { Component } from "react";
+import Temp from "./temp/temp";
+//import Counter from "./counter/counter";
+//import User from "./users/User";
+
 
 class Main extends Component {
-    // * state in class Component
-    /*   constructor(props) {
-        super(props);
-        this.state = {
-            users: [{ id: 1, name: "Reza" }],
-            counter: 0,
-        };
-        
-        // * render state
-         setTimeout(() => {
-            // eslint-disable-next-line react/no-direct-mutation-state
-            this.setState({
-                users: [{ id: 1, name: "ali" }],
-            });
-        }, 1000);
-
-        // * bind in function
-        //this.handleClick = this.handleClick.bind(this);
-    } */
-
-    // * even handler
-    /*  handleClick() {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    } */
     render() {
         return (
             <div>
-                {/*  <User {...this.state.users[0]} />
-                <br />
-                <h2>{this.state.counter}</h2>
-                <button onClick={this.handleClick.bind(this)}>click</button> */}
-                <Counter />
+                <Temp />
             </div>
         );
     }
