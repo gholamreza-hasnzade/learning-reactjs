18fa6585085ce868e0d25a1cc35f88ebf5732370
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-10 12:01:54 +0330

    Notification and npm i react-bootstrap

--- src/class-components/index.js
+++ src/class-components/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Rendring from "./rendring/rendring";
+import Notification from "./notification/notification";
 //import Params from "./params/params";
 //import Temp from "./temp/temp";
 //import Counter from "./counter/counter";
 //import User from "./users/User";
 
+
+
 class Main extends Component {
     render() {
-        return <Rendring />;
+        return <Notification />;
     }
 }
 export default Main;
