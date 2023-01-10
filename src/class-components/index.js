78ec23cce1897e3ac9f6989d8ec7069e065f0e0c
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-10 09:45:57 +0330

    render state

--- src/class-components/index.js
+++ src/class-components/index.js
@@ -6,15 +6,23 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [
-                { id: 1, name: "Reza" },
-                { id: 2, name: "MMad" },
-            ],
+            users: [{ id: 1, name: "Reza" }],
         };
+        // * render state
+        setTimeout(() => {
+            // eslint-disable-next-line react/no-direct-mutation-state
+            this.setState({
+                users: [{ id: 1, name: "ali" }],
+            });
+        }, 1000);
     }
 
     render() {
-        return <User {...this.state.users[0]} />;
+        return (
+            <div>
+                <User {...this.state.users[0]} />
+            </div>
+        );
     }
 }
 export default Main;
