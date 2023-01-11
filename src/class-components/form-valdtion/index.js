df15008deb4af5ee2a951aca01231354ca9290b2
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-11 10:50:20 +0330

    validate register form

--- src/class-components/form-valdtion/index.js
+++ src/class-components/form-valdtion/index.js
@@ -13,18 +13,30 @@ class FormValdtion extends Component {
         };
     }
     handleChange(evt, field) {
-        console.log(evt.target.value, field);
         // check it out: we get the evt.target.name (which will be either "email" or "password")
         // and use it to target the key on our `state` object with the same name, using bracket syntax
         this.setState({ [field]: evt.target.value });
     }
     submitHandler = (e) => {
         e.preventDefault();
+        const { firstNameData, lastNameData, emailData } = this.state;
         this.setState({ submitted: true });
+        if (
+            firstNameData.length !== 0 &&
+            lastNameData.length !== 0 &&
+            emailData.length !== 0
+        ) {
+            this.setState({ allValid: true });
+
+            setTimeout(() => {
+                this.setState({ allValid: false });
+            }, 2000);
+        }
+
         // console.log(this.state.firstNameData, this.state.lastNameData);
     };
     render() {
-        const { firstNameData, lastNameData, emailData, submitted } =
+        const { firstNameData, lastNameData, emailData, submitted, allValid } =
             this.state;
         return (
             <div className="form-container">
@@ -33,7 +45,12 @@ class FormValdtion extends Component {
                     onSubmit={this.submitHandler}
                     autoComplete="off"
                 >
-                    {/* <div className="success-message">Success! Thank you for registering</div> */}
+                    {allValid && (
+                        <div className="success-message">
+                            Success! Thank you for registering
+                        </div>
+                    )}
+
                     <input
                         id="first-name"
                         className="form-field"
