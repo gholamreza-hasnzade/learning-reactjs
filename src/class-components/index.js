1a08f12197f4ab957e7ecebcf4e08d0d1a5c74f3
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-14 08:58:31 +0330

    comemend method

--- src/class-components/index.js
+++ src/class-components/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //import FormValdtion from "./form-valdtion";
 //import GuizeApp from "./quize-app";
 //import Ticket from "./ticket";
-import Method from "./method";
+//import Method from "./method";
 //import Rendring from "./rendring/rendring";
 //import Form from "./from/Form";
 //import Notification from "./notification/notification";
@@ -12,35 +12,11 @@ import Method from "./method";
 //import User from "./users/User";
 
 class Main extends Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [
-                { id: 1, name: "Reza" },
-                { id: 2, name: "Mammd" },
-                { id: 3, name: "jafar" },
-            ],
-        };
-    }
-    userRemverHandler = (userId) => {
-        let newUsers = [...this.state.users];
-        let mainUserInedex = newUsers.findIndex((user) => user.id === userId);
-        newUsers.splice(mainUserInedex, 1);
-        this.setState({
-            users: newUsers,
-        });
-    };
+   
     render() {
-        const { users } = this.state;
         return (
             <div>
-                {users.map((user) => (
-                    <Method
-                        key={user.id}
-                        {...user}
-                        onRemove={this.userRemverHandler}
-                    />
-                ))}
+               
             </div>
         );
     }
--- src/class-components/method/index.js
+++ src/class-components/method/index.js
@@ -14,3 +14,38 @@ export class Method extends Component {
 }
 
 export default Method;
+
+/* 
+ constructor() {
+        super();
+        this.state = {
+            users: [
+                { id: 1, name: "Reza" },
+                { id: 2, name: "Mammd" },
+                { id: 3, name: "jafar" },
+            ],
+        };
+    }
+    userRemverHandler = (userId) => {
+        let newUsers = [...this.state.users];
+        let mainUserInedex = newUsers.findIndex((user) => user.id === userId);
+        newUsers.splice(mainUserInedex, 1);
+        this.setState({
+            users: newUsers,
+        });
+    };
+
+
+
+      const { users } = this.state;
+        return (
+            <div>
+                {users.map((user) => (
+                    <Method
+                        key={user.id}
+                        {...user}
+                        onRemove={this.userRemverHandler}
+                    />
+                ))}
+            </div>
+*/
