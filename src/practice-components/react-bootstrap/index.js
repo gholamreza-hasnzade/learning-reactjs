c24145a111f3bcb5f67400c1b4b04588b82e6be4
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-15 19:12:51 +0330

    show aleart

--- src/practice-components/react-bootstrap/index.js
+++ src/practice-components/react-bootstrap/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Alert, Button, Stack } from "react-bootstrap";
 
 export const ReactBootstrap = () => {
-  return (
+  /*  return (
     <div>
       <Stack direction="horizontal" gap={2}>
         <Button as="a" variant="primary">
@@ -28,5 +28,21 @@ export const ReactBootstrap = () => {
         </Alert>
       ))}
     </div>
-  );
+  ); */
+
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert>
+    );
+  }
+  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
 };
