378dc0208f084a33f1eea446d1b9a4fd49c40e43
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-15 18:36:08 +0330

    clean code in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import Main from "./class-components";
-//import ProductCart from "./practice-components/product-cart";
 
 export class App extends Component {
     render() {
         return (
             <>
-            <Main message={"this is text"}/>
-              {/*   <ProductCart /> */}
+           
             </>
         );
     }
