78361a7d9b959ea29416cdf9c7fa2708b6e4791d
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-16 19:03:37 +0330

    doplacte card

--- src/practice-components/react-bootstrap/index.js
+++ src/practice-components/react-bootstrap/index.js
@@ -47,16 +47,30 @@ export const ReactBootstrap = () => {
   return <Button onClick={() => setShow(true)}>Show Alert</Button>; */
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={Album_4} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="container m-5">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={Album_4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={Album_4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
