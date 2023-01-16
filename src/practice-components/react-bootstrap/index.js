18dcac233aba775d212f24cec1a071662c1d39c9
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-16 19:06:36 +0330

    diffreant bg cololr

--- src/practice-components/react-bootstrap/index.js
+++ src/practice-components/react-bootstrap/index.js
@@ -60,7 +60,7 @@ export const ReactBootstrap = () => {
         </Card.Body>
       </Card>
 
-      <Card style={{ width: "18rem" }}>
+      <Card bg="success" style={{ width: "18rem" }}>
         <Card.Img variant="top" src={Album_4} />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
@@ -71,6 +71,37 @@ export const ReactBootstrap = () => {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
+
+
+      <>
+      {[
+        'Primary',
+        'Secondary',
+        'Success',
+        'Danger',
+        'Warning',
+        'Info',
+        'Light',
+        'Dark',
+      ].map((variant) => (
+        <Card
+          bg={variant.toLowerCase()}
+          key={variant}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '18rem' }}
+          className="mb-2"
+        >
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
     </div>
   );
 };
