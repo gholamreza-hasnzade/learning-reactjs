d758a3bdd4e621791968a127cf718e5e78e01f7e
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-23 11:22:01 +0330

    coment code and import TodoList

--- src/fun-components/index.js
+++ src/fun-components/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import State from "./state";
-//import TodoList from "./todolists/TodoList";
+//import State from "./state";
+import TodoList from "./todolists/TodoList";
 //import Shop from "./shop/Shop";
 
 const Parctice = () => {
     return (
         <div>
-           <State />
+           <TodoList />
         </div>
     );
 };
