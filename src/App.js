1c279b55ae1b2baf320f472fd2b31be86e96a4c9
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-28 08:46:53 +0330

    clean code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Parctice from "./fun-components";
+//import Parctice from "./fun-components";
 //import Main from "./class-components";
 
 export class App extends Component {
     render() {
         return (
             <>
-                <Parctice />
+                
             </>
         );
     }
