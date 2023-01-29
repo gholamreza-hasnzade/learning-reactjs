bc481b1dd85a218729e3f6060d551e2c46caaf16
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-29 08:49:50 +0330

    change folder name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Posts from "./components/Posts";
-import Post from "./components/Post";
+/* import Home from "./components/learn-rrd/Home";
+import Contact from "./components/learn-rrd/Contact";
+import About from "./components/learn-rrd/About";
+import Posts from "./components/learn-rrd/Posts";
+import Post from "./components/learn-rrd/Post"; */
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Home />} path="/" />
+              {/*   <Route element={<Home />} path="/" />
                 <Route element={<Contact />} path="/contact" />
                 <Route element={<About />} path="/about" />
                 <Route element={<Posts />} path="/posts" />
                 <Route element={<Post />} path="/post/:id" />
-                <Route element={<h1>404</h1>} path="*" />
+                <Route element={<h1>404</h1>} path="*" /> */}
             </Routes>
         </BrowserRouter>
     );
