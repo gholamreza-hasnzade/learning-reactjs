bdf7e4ae39fb475932646d50f76a30f7c3714d83
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-29 09:36:21 +0330

    create main course

--- src/components/sabzlearn/index.js
+++ src/components/sabzlearn/index.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Courses from "./Courses";
 import About from "./About";
+import MainCourse from "./MainCourse";
 
 const Sablearn = () => {
     return (
@@ -11,6 +12,7 @@ const Sablearn = () => {
             <Routes>
                 {" "}
                 <Route path="/courses" element={<Courses />} />
+                <Route path="/course/:courseID" element={<MainCourse />} />
                 <Route path="/about/*" element={<About />} />
             </Routes>
         </>
