14eb4943ad66005ecd9abbb5206d7ac0f9d9d9bc
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-30 21:42:25 +0330

    clean code app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import React from "react";
 
 import {  BrowserRouter } from "react-router-dom";
 import Sablearn from "./components/sabzlearn";
-/* import Home from "./components/learn-rrd/Home";
-import Contact from "./components/learn-rrd/Contact";
-import About from "./components/learn-rrd/About";
-import Posts from "./components/learn-rrd/Posts";
-import Post from "./components/learn-rrd/Post"; */
 
 const App = () => {
     return (
         <BrowserRouter>
             <Sablearn />
-            {/* <Routes>
-                  <Route element={<Home />} path="/" />
-                <Route element={<Contact />} path="/contact" />
-                <Route element={<About />} path="/about" />
-                <Route element={<Posts />} path="/posts" />
-                <Route element={<Post />} path="/post/:id" />
-                <Route element={<h1>404</h1>} path="*" />
-            </Routes> */}
         </BrowserRouter>
     );
 };
